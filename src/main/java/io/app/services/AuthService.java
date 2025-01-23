4ed1b0079ac36a47ec3fd6e45f32c9792e8a713a
jhantu626 <jhantubala626@gmail.com>
2025-01-23 12:36:31 +0530

    Remove YAML configuration files from Git tracking

--- src/main/java/io/app/services/AuthService.java
+++ src/main/java/io/app/services/AuthService.java
@@ -4,9 +4,10 @@ package io.app.services;
 import io.app.dto.ApiResponse;
 import io.app.dto.ResponseToken;
 import io.app.model.Teacher;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface AuthService {
     public ApiResponse login(String phoneNumber);
-    public ApiResponse signup(Teacher teacher);
+    public ApiResponse signup(Teacher teacher, MultipartFile profilePic);
     public ResponseToken validateToken(String phoneNumber,String otp);
 }
