ae0e224932762c2570e0613a5232d708d12220ce
jhantu626 <jhantubala626@gmail.com>
2025-02-05 17:51:54 +0530

    added cors for live server

--- src/main/java/io/app/config/CorsConfig.java
+++ src/main/java/io/app/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply to all paths
-                .allowedOrigins("http://127.0.0.1:5173", "http://localhost:9097") // Allowed origins
+                .allowedOrigins("http://127.0.0.1:5173", "http://localhost:9097","http://localhost:4173","https://fees.busketbell.in/") // Allowed origins
                 .allowedMethods("*") // Allow all HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow sending cookies/credentials (important if using auth)
--- src/main/java/io/app/config/SecurityConfig.java
+++ src/main/java/io/app/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://fees.busketbell.in/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH")); // Allowed HTTP methods
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
